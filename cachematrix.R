e2dc2f98258ee53cce2d3501beecc43d2cc349e4
Khaled KamaL <kkamal@moi.gov.qa>
2016-01-25 08:17:44 +0300

    Comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix stores and returns 4 functions: set , get , setinverse, and getinverse They are briefly described below:
+## set - retrieves new matrix, sets Cache to NULL
+## get - returns the stored matrix
+## setinverse - puts solved matrix into Cache
+## getinverse - retrieves Cache
 
 makeCacheMatrix <- function(x = numeric()) {
         inv <- NULL
@@ -17,7 +18,7 @@ makeCacheMatrix <- function(x = numeric()) {
              getinverse = getinverse)
 }
 
-## Write a short comment describing this function
+## This function checks if the Cache is null- if not null, then it gets the cached value, if null, then in calculates the inveserse 
 
 cacheSolve <- function(x, ...) {
         inv <- x$getinverse()
